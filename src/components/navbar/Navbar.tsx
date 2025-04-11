758395d7c0e3b293edca5391bca8ee56b1b58d61
DiasRay <diassilvarayanne@gmail.com>
2025-04-11 08:34:46 -0300

    CRUD Tema

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -23,7 +23,10 @@ function Navbar() {
           </Link>
 
           <div className="flex gap-4">
-            Postagens Temas Cadastrar tema Perfil
+            <Link to='/postagens' className='hover:underline'>Postagens</Link> 
+            <Link to='/temas' className='hover:underline'>Temas</Link>
+            <Link to='/cadastrartema' className='hover:underline'>Cadastrar tema</Link> 
+            Perfil
             <Link to="" onClick={logout} className="hover:underline">
               Sair
             </Link>

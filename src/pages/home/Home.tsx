e40b8db7b54d7e8a2c0fe2f9a8dc615c545ae381
DiasRay <diassilvarayanne@gmail.com>
2025-04-14 14:17:28 -0300

    Postagens

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,18 @@
+import ListaPostagens from "../../components/postagens/listapostagens/ListaPostagens";
+import ModalPostagem from "../../components/postagens/modalpostagens/ModalPostagem";
 
 function Home() {
     return (
         <>
             <div className="bg-indigo-900 flex justify-center">
-                <div className='container grid grid-cols-2 text-white'>
+                <div className="container grid grid-cols-2 text-white">
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
-                        <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
-                        </h2>
-                        <p className='text-xl'>
-                            Expresse aqui seus pensamentos e opniões
-                        </p>
+                        <h2 className="text-5xl font-bold">Seja Bem Vinde!</h2>
+                        <p className="text-xl">Expresse aqui seus pensamentos e opniões</p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'
-                                >
-                                Nova Postagem
+                            <div className="flex justify-around gap-4">
+                                <ModalPostagem />
                             </div>
                         </div>
                     </div>
@@ -25,13 +21,14 @@ function Home() {
                         <img
                             src="https://i.imgur.com/fyfri1v.png"
                             alt="Imagem Página Home"
-                            className='w-2/3'
+                            className="w-2/3"
                         />
                     </div>
                 </div>
             </div>
+            <ListaPostagens />
         </>
-    )
+    );
 }
 
-export default Home
+export default Home;
